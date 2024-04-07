b564f9137100a94374c47c2fe3da2230ef5b7134
Abhay Khator <abhaykhator69@gmail.com>
2024-04-07 11:16:16 +0530

    email

--- app/src/main/java/com/example/healthiswealth/MainActivity.java
+++ app/src/main/java/com/example/healthiswealth/MainActivity.java
@@ -8,6 +8,9 @@ import android.widget.Button;
 import android.widget.TextView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import javax.mail.*;
+import javax.mail.internet.*;
+import java.util.Properties;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -62,6 +65,41 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    public class EmailService {
+        public static void sendLoginNotification(String userEmail) {
+            final String username = ""; // Your email address
+            final String password = ""; // Your email password
+
+            Properties props = new Properties();
+            props.put("mail.smtp.auth", "true");
+            props.put("mail.smtp.starttls.enable", "true");
+            props.put("mail.smtp.host", "smtp.gmail.com"); // Use Gmail SMTP server
+            props.put("mail.smtp.port", "587");
+
+            Session session = Session.getInstance(props,
+                    new javax.mail.Authenticator() {
+                        protected PasswordAuthentication getPasswordAuthentication() {
+                            return new PasswordAuthentication(username, password);
+                        }
+                    });
+
+            try {
+                Message message = new MimeMessage(session);
+                message.setFrom(new InternetAddress(username));
+                message.setRecipients(Message.RecipientType.TO,
+                        InternetAddress.parse(userEmail));
+                message.setSubject("Login Notification");
+                message.setText("Dear User,\n\nSomeone has logged into your account.\n\nRegards,\nYour App Team");
+
+                Transport.send(message);
+
+                System.out.println("Login notification email sent to: " + userEmail);
+            } catch (MessagingException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
 }
